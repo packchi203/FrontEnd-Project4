e93560b67462a2ead84483834eb90a85a9af7a87
packchi203 <96831035+packchi203@users.noreply.github.com>
2023-11-19 19:12:12 +0700

    admin post updata

--- pages/admin/posts/edit_post.tsx
+++ pages/admin/posts/edit_post.tsx
@@ -1,42 +1,62 @@
-// edit_post.tsx
-
 import React, { useState } from 'react';
-interface Post {
-    id: number;
-    title: string;
-    slug: string;
-    tags: { name: string }[];
-    content: string;
-    account: { name: string };
-    commentCount: number;
-    viewCount: number;
-    voteCount: number;
-    createdAt: string;
-  }
+import toast from 'react-hot-toast';
+import styles from './edit_account.module.css'; 
 
+export interface Post{
+  id: number;
+  title: string;
+  slug: string;
+  tags: { name: string }[];
+  content: string;
+  account: { name: string };
+  commentCount: number;
+  viewCount: number;
+  voteCount: number;
+  createdAt: string;
+}
 interface EditPostFormProps {
   post: Post;
   onCancel: () => void;
-  onSubmit: (updatedPost: Post) => void;
+  onSubmit: (updatedPost: Post) => Promise<void>; 
 }
 
 const EditPostForm: React.FC<EditPostFormProps> = ({ post, onCancel, onSubmit }) => {
   const [updatedPost, setUpdatedPost] = useState<Post>({ ...post });
-
+  const [error, setError] = useState<string | null>(null);
   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const { name, value } = e.target;
     setUpdatedPost((prevPost) => ({ ...prevPost, [name]: value }));
   };
 
-  const handleSubmit = (e: React.FormEvent) => {
+  
+const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    onSubmit(updatedPost);
+
+    if (Object.values(updatedPost).some((value) => {
+      if (typeof value === 'string') {
+        return !value.trim(); // Check if the string is empty or consists only of whitespace
+      }
+      return false; // For non-string values
+    })) {
+      toast.error('Vui lòng điền đầy đủ thông tin.', {
+        icon: '❌',
+      });
+      return;
+    }
+  
+
+    // Reset error message
+    setError(null);
+
+    // Call the onSubmit function
+    await onSubmit(updatedPost);
   };
 
   return (
-    <div style={modalStyles}>
-      <form onSubmit={handleSubmit} style={formStyles}>
-        {/* Add your form fields here, e.g., title, content, etc. */}
+    <div className={styles.editUserModal} style={modalStyles}>
+      
+      <form onSubmit={handleSubmit} className={styles.editUserForm} style={formStyles}>
+      {error && <p className={styles.error}>{error}</p>}
         <label htmlFor="title">Title:</label>
         <input
           type="text"
@@ -45,12 +65,37 @@ const EditPostForm: React.FC<EditPostFormProps> = ({ post, onCancel, onSubmit })
           value={updatedPost.title}
           onChange={handleChange}
         />
-        {/* Add other form fields as needed */}
 
-        <button type="submit">Update Post</button>
-        <button type="button" onClick={onCancel}>
-          Cancel
-        </button>
+        <label htmlFor="slug">Slug:</label>
+        <input
+          type="text"
+          id="slug"
+          name="slug"
+          value={updatedPost.slug}
+          onChange={handleChange}
+        />
+          <label htmlFor="postContent">Content:</label>
+          <textarea
+            id="postContent"
+            name="content"
+            value={updatedPost.content}
+            onChange={handleChange}
+            rows={4} // You can adjust the number of rows as needed
+            style={{
+              width: '100%', 
+              height: '350px',// Set the width to 100% or adjust as needed
+              padding: '8px', // Adjust padding as needed
+              boxSizing: 'border-box', // Include padding in the total width/height
+              resize: 'vertical', // Allow vertical resizing
+            }}
+        />
+
+       
+
+        <div className={styles.buttonContainer}>
+        <button type="button" onClick={onCancel}>Cancel</button>
+          <button type="submit">Update Post</button>
+        </div>
       </form>
     </div>
   );
@@ -63,6 +108,7 @@ const modalStyles: React.CSSProperties = {
   transform: 'translate(-50%, -50%)',
   backgroundColor: 'white',
   padding: '20px',
+  width: '500px',
   boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
 };
 
--- pages/admin/posts/index.tsx
+++ pages/admin/posts/index.tsx
@@ -1,10 +1,15 @@
 
 import { useRouter } from 'next/router';
 import React, { useState, useEffect } from 'react';
-import { DataTable } from './data-table';
-import { AdminLayout } from '@/components/layouts'
-import { NextPageWithLayout, PostModel } from '@/models'
+import { AdminLayout } from '@/components/layouts';
+import { NextPageWithLayout } from '@/models';
 import EditPostForm from './edit_post';
+import ConfirmationBox from './confirm';
+
+import styles from './edit_account.module.css'; 
+import toast from 'react-hot-toast';
+
+
 interface Post {
   id: number;
   title: string;
@@ -19,39 +24,55 @@ interface Post {
 }
 const Home: NextPageWithLayout = () => {
   const [data, setData] = useState<Post[]>([]);
-  const [token] = useState<string>('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJiYWNoY2hpMjAwM3ZuQGdtYWlsLmNvbSIsInJvbGUiOiJBRE1JTiIsImlzcyI6IkFQVEVDSCIsImV4cCI6MTcwMDM4NDA0Mn0.GiolGkFY74uweBihRIUSoO96I2YQteLbPQOM5eBDtUE');
-  const [showEditForm, setShowEditForm] = useState(false); // Add this line
-  const [editPost, setEditPost] = useState<Post | null>(null); // Add this line
+  const [token] = useState<string>('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJiYWNoY2hpMjAwM3ZuQGdtYWlsLmNvbSIsInJvbGUiOiJBRE1JTiIsImlzcyI6IkFQVEVDSCIsImV4cCI6MTcwMDk4OTA5MX0.KWqbqJo2fH2KeZj7tzMm6N01wpv6lId4931sYv3ACyU');
+  const [showEditForm, setShowEditForm] = useState(false);
+  const [editPost, setEditPost] = useState<Post | null>(null);
+  const [searchTerm, setSearchTerm] = useState<string>('');
+  const [filteredData, setFilteredData] = useState<Post[]>([]); 
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [rowsPerPage, setRowsPerPage] = useState<number>(10);
   const router = useRouter();
-    useEffect(() => {
-      fetchData();
-    }, []);
-    const fetchData = async () => {
-      try {
-       
-        const response = await fetch('http://localhost:8080/api/posts/list', {
-          headers: {
-            Authorization: `Bearer ${token}`, // Include the authentication token in the request headers
-          },
-        });
-    
-        if (!response.ok) {
-          // Handle error response
-          console.error(`Error fetching data: ${response.statusText}`);
-          return;
-        }
-    
-        const result: Post[] = await response.json();
-        // Đảo ngược danh sách trước khi cập nhật state
-        setData(result.reverse());
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
+  const totalPages = Math.ceil(filteredData.length / rowsPerPage);
+  const startIndex = (currentPage - 1) * rowsPerPage;
+  const endIndex = startIndex + rowsPerPage;
+  const visibleData = filteredData.slice(startIndex, endIndex);
+  const [showConfirmationBox, setShowConfirmationBox] = useState(false);
+  const [selectedPostId, setSelectedPostId] = useState<number | null>(null);
+  const [showCreateForm, setShowCreateForm] = useState(false);
 
-    const handleDelete = async (postId: number) => {
+  
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/api/posts/list', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (!response.ok) {
+        console.error(`Error fetching data: ${response.statusText}`);
+        return;
+      }
+      const result: Post[] = await response.json();
+      setData(result.reverse());
+        setFilteredData(result.reverse());
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+  const handleDelete = (postId: number) => {
+    setSelectedPostId(postId);
+    setShowConfirmationBox(true);
+  };
+  const performDelete = async () => {
+    // Perform the delete operation using the selected post ID
+    if (selectedPostId !== null) {
       try {
-        const response = await fetch(`http://localhost:8080/api/admin/posts/${postId}`, {
+        const response = await fetch(`http://localhost:8080/api/admin/posts/${selectedPostId}`, {
           method: 'DELETE',
           headers: {
             Authorization: `Bearer ${token}`,
@@ -59,102 +80,265 @@ const Home: NextPageWithLayout = () => {
         });
   
         if (!response.ok) {
-          console.error(`Error deleting post: ${response.statusText}`);
+          if (response.status === 403) {
+            // Display a specific alert if the status is 403
+            toast.error(`Community này đã có bài viết không thể xoá`, {
+              icon: '❌',
+            })
+          } else {
+            // Display error toast if deletion fails for other reasons
+            toast.error(`Error deleting post: ${response.statusText}`, {
+              icon: '❌',
+            });
+          }
           return;
         }
   
-        // Xóa bài viết khỏi danh sách khi xóa thành công
-        setData((prevData) => prevData.filter((post) => post.id !== postId));
-      } catch (error) {
-        console.error('Error deleting post:', error);
-      }
-    };
-    
-    const handleEdit = (postId: number) => {
-      const postToEdit = data.find((post) => post.id === postId);
-      if (postToEdit) {
-        setEditPost(postToEdit);
-        setShowEditForm(true);
-      }
-    };
-    const handleEditSubmit = async (updatedPost: Post) => {
-      try {
-        const response = await fetch(`http://localhost:8080/api/admin/posts/${updatedPost.id}`, {
-          method: 'PUT', // Use PUT for updating the entire resource
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${token}`,
-          },
-          body: JSON.stringify(updatedPost),
-        });
-    
-        if (!response.ok) {
-          console.error(`Error updating post: ${response.statusText}`);
-          return;
-        }
-    
-        // Update the data array with the updated post
-        setData((prevData) =>
-          prevData.map((post) => (post.id === updatedPost.id ? updatedPost : post))
+        // Remove the deleted post from both data and filteredData
+        setData((prevData) => prevData.filter((post) => post.id !== selectedPostId));
+        setFilteredData((prevFilteredData) =>
+          prevFilteredData.filter((post) => post.id !== selectedPostId)
         );
-    
-        // Hide the edit form
-        setShowEditForm(false);
+  
+        // Display success toast
+        toast.success('Post deleted successfully!', {
+          icon: '✅',
+        });
       } catch (error) {
-        console.error('Error updating post:', error);
+        // Display error toast if an exception occurs
+        console.error('Error deleting post:', error);
+        toast.error('An error occurred while deleting the post.', {
+          icon: '❌',
+        });
+      } finally {
+        // Reset the selected post ID
+        setSelectedPostId(null);
       }
+    }
+  };
+  
+  const handleConfirmationCancel = () => {
+    // Hide the confirmation box and reset the selected post ID
+    setShowConfirmationBox(false);
+    setSelectedPostId(null);
+  };
+  const handleConfirmationConfirm = () => {
+    performDelete();
+    // Hide the confirmation box
+    setShowConfirmationBox(false);
+  };
+  const handleCreateSubmit = async (newPost: Post) => {
+    try {
+      const response = await fetch('http://localhost:8080/api/admin/posts', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(newPost),
+      });
+
+      if (!response.ok) {
+        // Display error toast if the creation fails
+        toast.error(`Error creating post: ${response.statusText}`, {
+          icon: '❌',
+        });
+        return;
+      }
+
+      // Update the data and filteredData with the created post
+      setData((prevData) => [newPost, ...prevData]);
+      setFilteredData((prevFilteredData) => [newPost, ...prevFilteredData]);
+
+      // Display success toast
+      toast.success('Post created successfully!', {
+        icon: '✅',
+      });
+
+      // Close the create form
+      setShowCreateForm(false);
+    } catch (error) {
+      // Display error toast if an exception occurs
+      console.error('Error creating post:', error);
+      toast.error('An error occurred while creating the post.', {
+        icon: '❌',
+      });
+    }
+  };
+
+  const handleEdit = (postId: number) => {
+    const postToEdit = data.find((post) => post.id === postId);
+    if (postToEdit) {
+      setEditPost(postToEdit);
+      setShowEditForm(true);
+    }
+  };
+  const handleEditSubmit = async (updatedPost: Post) => {
+    try {
+      const response = await fetch(`http://localhost:8080/api/admin/posts/${updatedPost.id}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(updatedPost),
+      });
+  
+      if (!response.ok) {
+        // Display error toast if the update fails
+        toast.error(`Error updating post: ${response.statusText}`, {
+          icon: '❌',
+        });
+        return;
+      }
+  
+      // Update the data and filteredData with the updated post
+      setData((prevData) =>
+        prevData.map((post) => (post.id === updatedPost.id ? updatedPost : post))
+      );
+      setFilteredData((prevFilteredData) =>
+        prevFilteredData.map((post) => (post.id === updatedPost.id ? updatedPost : post))
+      );
+  
+      // Display success toast
+      toast.success('Post updated successfully!', {
+        icon: '✅',
+      });
+  
+      // Close the edit form
+      setShowEditForm(false);
+    } catch (error) {
+      // Display error toast if an exception occurs
+      console.error('Error updating post:', error);
+      toast.error('An error occurred while updating the post.', {
+        icon: '❌',
+      });
+    }
+  };
+  const handleSearch = () => {
+    const filtered = data.filter((post) =>
+      post.slug.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilteredData(filtered);
+  };
+  const handleRowsPerPageChange = (value: number) => {
+    setRowsPerPage(value);
+    setCurrentPage(1); // Reset current page when changing rows per page
+  };
+  const handlePreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+  const formatDate = (dateString: string): string => {
+    const options: Intl.DateTimeFormatOptions = {
+      year: 'numeric',
+      month: 'numeric',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
     };
-    
-    
+    return new Date(dateString).toLocaleDateString(undefined, options);
+  };
   return (
-    <div>
-    <h1>Post List</h1>
-    <table>
-      <thead>
-        <tr>
-          <th>Title</th>
-          <th>Tags</th>
-          <th>Author</th>
-          <th>Comment Count</th>
-          <th>View Count</th>
-          <th>Vote Count</th>
-          <th>Created At</th>
-          <th>Action</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((post) => (
-          <tr key={post.id}>
-            <td>{post.title}</td>
-            <td>{post.tags.map((tag) => tag.name).join(', ')}</td>
-            <td>{post.account.name}</td>
-            <td>{post.commentCount}</td>
-            <td>{post.viewCount}</td>
-            <td>{post.voteCount}</td>
-            <td>{post.createdAt}</td>
-            <td>
-            <button onClick={() => router.push(`/bai-dang/${post.slug}`)}>View Post</button>
-              <button onClick={() => handleEdit(post.id)}>Edit</button>
-              <button onClick={() => handleDelete(post.id)}>Delete</button>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-      {/* Add the following code to render the edit form conditionally */}
-      {showEditForm && editPost && (
-      <EditPostForm
-        post={editPost}
-        onCancel={() => setShowEditForm(false)}
-        onSubmit={handleEditSubmit}
-      />
-    )}
+      <div className={styles.formContainer} >
+              <h1>Post List</h1>
+            <div  className={styles.topTable}  >
+            {showConfirmationBox && (
+                  <div className={`${styles.confirmationBoxContainer} ${styles.backdropFilter}`}>
+                    <ConfirmationBox onCancel={handleConfirmationCancel} onConfirm={handleConfirmationConfirm} />
+                  </div>
+                )}
+
+              <div className={styles.searchContainer}>
+                <input
+                  placeholder='Search by Name:'
+                  type="text"
+                  id="search"
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <button className={styles.searchButton} onClick={handleSearch}>Search</button>
+              </div>
+              <div className={styles.rowsPerPageContainer}>
+                <label htmlFor="rowsPerPage">Rows per page:</label>
+                <select
+                  id="rowsPerPage"
+                  value={rowsPerPage}
+                  onChange={(e) => handleRowsPerPageChange(Number(e.target.value))}
+                >
+                  <option value={10}>10</option>
+                  <option value={25}>25</option>
+                  <option value={50}>50</option>
+                  <option value={100}>100</option>
+                  <option value={200}>200</option>
+                </select>
+              </div>
+            </div>
+
+        <table className={styles.myTable}>
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Community</th>
+                <th>Author</th>
+                <th>Comment</th>
+                <th>View</th>
+                <th>Vote</th>
+                <th>Create At</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+            {visibleData.map((post) => (
+                <tr key={post.id}>
+                  <td>{post.title}</td>
+                  <td>{post.tags.map(tag => tag.name).join(', ')}</td>
+                  <td>{post.account.name}</td>
+                  <td>{post.commentCount}</td>
+                  <td>{post.viewCount}</td>
+                  <td>{post.voteCount}</td>
+                  <td>{formatDate(post.createdAt)}</td>
+                  <td>
+                  <div className={styles.dropdown}>
+                      <button onClick={() => router.push(`/bai-dang/${post.slug}`)}>View | </button>
+                      <button onClick={() => handleEdit(post.id)}> Edit | </button>
+                      <button onClick={() => handleDelete(post.id)}> Delete</button>
+                  </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <div className={styles.pagination} >
+            <button onClick={handlePreviousPage} disabled={currentPage === 1}>
+              Previous
+            </button>
+            <span>{` ${currentPage} / ${totalPages}`}</span>
+            <button onClick={handleNextPage} disabled={currentPage === totalPages}>
+              Next
+            </button>
+          </div>
+          {showEditForm && editPost && (
+            <EditPostForm
+              post={editPost}
+              onCancel={() => setShowEditForm(false)}
+              onSubmit={handleEditSubmit}
+            />
+          )}
+    
   </div>
   );
-}
+};
 
-Home.Layout = AdminLayout
-Home.sidebarRight = true
-Home.SidebarLeft = true
-Home.requestAuth = false
-export default Home
+Home.Layout = AdminLayout;
+Home.sidebarRight = true;
+Home.SidebarLeft = true;
+Home.requestAuth = false;
+export default Home;
